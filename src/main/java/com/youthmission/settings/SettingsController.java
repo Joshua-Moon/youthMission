5ea9438644ec67142efb311aaf1e926bddd6f4ab
Joshua-Moon <joshua8565@gmail.com>
2020-06-06 22:13:58 +0900

    29_update_password_test

--- src/main/java/com/youthmission/settings/SettingsController.java
+++ src/main/java/com/youthmission/settings/SettingsController.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -18,8 +20,16 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class SettingsController {
 
+    @InitBinder("passwordForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(new PasswordFormValidator());
+    }
+
     static final String SETTINGS_PROFILE_VIEW_NAME = "settings/profile";
     static final String SETTINGS_PROFILE_URL = "/settings/profile";
+
+    static final String SETTINGS_PASSWORD_VIEW_NAME = "settings/password";
+    static final String SETTINGS_PASSWORD_URL = "/settings/password";
 
     private final AccountService accountService;
 
@@ -43,6 +53,25 @@ public class SettingsController {
         return "redirect:" + SETTINGS_PROFILE_URL;
     }
 
+    @GetMapping(SETTINGS_PASSWORD_URL)
+    public String updatePasswordForm(@CurrentUser Account account, Model model){
+        model.addAttribute(account);
+        model.addAttribute(new PasswordForm());
+        return SETTINGS_PASSWORD_VIEW_NAME;
+    }
+
+    @PostMapping(SETTINGS_PASSWORD_URL)
+    public String updatePassword(@CurrentUser Account account, @Valid PasswordForm passwordForm, Errors errors,
+                                 Model model, RedirectAttributes attributes){
+        if (errors.hasErrors()) {
+            model.addAttribute(account);
+            return SETTINGS_PASSWORD_VIEW_NAME;
+        }
+
+        accountService.updatePassword(account, passwordForm.getNewPassword());
+        attributes.addFlashAttribute("message", "패스워드를 변경했습니다.");
+        return "redirect:" + SETTINGS_PASSWORD_URL;
+    }
 
 
 }
